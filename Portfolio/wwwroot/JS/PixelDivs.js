5a9cf9e373455681f1a0a94f1b01b034a9549b1a
Nxbi <noradegreeuw@gmail.com>
2025-10-05 22:25:12 +0200

    alot of divs get generated now (dont know if its the right amount yet and their position isnt right)

---
+++ Portfolio/wwwroot/JS/PixelDivs.js
@@ -0,0 +1,14 @@
+﻿export function PixelOverlayDivs(divId, pixelBorderSize) {
+  let div = document.getElementById(divId);
+  if (!div) { console.warn("no div found"); return };
+  let clientRect = div.getBoundingClientRect();
+  let pixelXAxis = clientRect.width / pixelBorderSize;
+  let pixelYAxis = clientRect.height / pixelBorderSize;
+  for (let i = 0; i < pixelYAxis; i++) {
+    for (let j = 0; j < pixelXAxis; j++) {
+      let pixelDiv = document.createElement("div");
+      pixelDiv.style.position = "relative";
+      div.appendChild(pixelDiv);
+    }
+  }
+}
