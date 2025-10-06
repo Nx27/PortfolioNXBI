5a03823c439216758626f5f10ebf2a631b82b95c
Nxbi <37214@ma-web.nl>
2025-10-06 13:55:22 +0200

    floored the pixel axis calculation and added a width and height to the pixels

--- Portfolio/wwwroot/JS/PixelDivs.js
+++ Portfolio/wwwroot/JS/PixelDivs.js
@@ -2,13 +2,17 @@
   let div = document.getElementById(divId);
   if (!div) { console.warn("no div found"); return };
   let clientRect = div.getBoundingClientRect();
-  let pixelXAxis = clientRect.width / pixelBorderSize;
-  let pixelYAxis = clientRect.height / pixelBorderSize;
+  let pixelXAxis = Math.floor(clientRect.width / pixelBorderSize);
+  let pixelYAxis = Math.floor(clientRect.height / pixelBorderSize);
+  let container = document.createElement("Container");
+  div.appendChild(container);
   for (let i = 0; i < pixelYAxis; i++) {
     for (let j = 0; j < pixelXAxis; j++) {
       let pixelDiv = document.createElement("div");
-      pixelDiv.style.position = "relative";
-      div.appendChild(pixelDiv);
+      pixelDiv.style.width = "4px";
+      pixelDiv.style.height = "4px";
+      pixelDiv.style.zIndex = "-1";
+      container.appendChild(pixelDiv);
     }
   }
 }
