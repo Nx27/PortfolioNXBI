09fd140e1822c17e6a289aa6b54dbe8461bec155
Nxbi <noradegreeuw@gmail.com>
2025-10-13 12:25:54 +0200

    Dialog system now has css + StickyNote now supports out of website navigation

--- Portfolio/Logic/CS/ProjectDetails.cs
+++ Portfolio/Logic/CS/ProjectDetails.cs
@@ -9,7 +9,8 @@
     public readonly string ImageUrl;
     public readonly string ProgrammingLanguage;
     public readonly string Time;
-    public ProjectDetails(string name, string description, string url, string imageUrl, string programmingLanguage, string time)
+    public readonly string LinkToProject;
+    public ProjectDetails(string name, string description, string url, string imageUrl, string programmingLanguage, string time, string linkToProject)
     {
       Name = name;
       Description = description;
@@ -17,6 +18,7 @@
       ImageUrl = imageUrl;
       ProgrammingLanguage = programmingLanguage; 
       Time = time;
+      LinkToProject = linkToProject;
       AllProjects.Add(this);
     }
 
@@ -24,13 +26,13 @@
     {
       if (AllProjects.Count == 0)
       {
-        new ProjectDetails("The Journey", "My first console app in NodeJS", "TheJourney", "images/logos/Artboard1.svg", "JS", "1 Week");
-        new ProjectDetails("First basic website", "First web project I ever made", "TheJourney", "404", "HTML, CSS", "1 Week");
-        new ProjectDetails("ColorTD", "Small tower defense game", "ColorTD", "", "C#, UnityEngine, ", "6 Weeks");
-        new ProjectDetails("Hollow Knight Slice", "A copy of a small part of the game", "HollowKnightSlice", "404", "C#, UnityEngine", "4 Weeks");
-        new ProjectDetails("Siepie and Takkie", "An international collaboration project between Sweden and the Netherlands", "SiepieAndTakkieOriginal", "404", "C#, UnityEngine", "3 Weeks");
-        new ProjectDetails("Siepie and Takkie Dialog system", "Finishing the dialog system for the orgiginal", "SiepieAndTakkieDialogSystem", "images/projects/SiepieAndTakkieDialogDummy.png", "C#, UnityEngine", "6 Weeks");
-        new ProjectDetails("Portfolio", "My personal portfolio website", "Portfolio", "404", "C#, ASP.NET Core, Blazor WASM, HTML, CSS", "2 Weeks");
+        new ProjectDetails("The Journey", "My first console app in NodeJS", "TheJourney", "images/logos/Artboard1.svg", "JS", "1 Week", "404");
+        new ProjectDetails("First basic website", "First web project I ever made", "TheJourney", "404", "HTML, CSS", "1 Week", "404");
+        new ProjectDetails("ColorTD", "Small tower defense game", "ColorTD", "", "C#, UnityEngine, ", "6 Weeks", "404");
+        new ProjectDetails("Hollow Knight Slice", "A copy of a small part of the game", "HollowKnightSlice", "404", "C#, UnityEngine", "4 Weeks", "404");
+        new ProjectDetails("Siepie and Takkie", "An international collaboration project between Sweden and the Netherlands", "SiepieAndTakkieOriginal", "404", "C#, UnityEngine", "3 Weeks", "404");
+        new ProjectDetails("Siepie and Takkie Dialog system", "Finishing the dialog system for the orgiginal", "SiepieAndTakkieDialogSystem", "images/projects/SiepieAndTakkieDialogDummy.png", "C#, UnityEngine", "6 Weeks", "https://github.com/Entropy-Entertainment/Siepie");
+        new ProjectDetails("Portfolio", "My personal portfolio website", "Portfolio", "404", "C#, ASP.NET Core, Blazor WASM, HTML, CSS", "2 Weeks", "404");
       }
       return AllProjects;
     }
