47c2e6b341f568fe3c9725b3dfa99878c5e7ffa2
Nxbi <146176774+Nx27-ma@users.noreply.github.com>
2025-11-01 11:40:53 +0100

    Added the base of all projects currently on the page

--- Portfolio/Logic/CS/ProjectDetails.cs
+++ Portfolio/Logic/CS/ProjectDetails.cs
@@ -27,13 +27,14 @@
     {
       if (AllProjects.Count == 0)
       {
-        new ProjectDetails("The Journey", "My first console app in NodeJS", "TheJourney", "images/logos/Artboard1.svg", new string[] { "JS" }, "1 Week", "404");
-        new ProjectDetails("First basic website", "First web project I ever made", "TheJourney", "404", new string[]  { "HTML, CSS" }, "1 Week", "404");
-        new ProjectDetails("ColorTD", "Small tower defense game", "ColorTD", "", new string[] { "C#, UnityEngine, " }, "6 Weeks", "404");
-        new ProjectDetails("Hollow Knight Slice", "A copy of a small part of the game", "HollowKnightSlice", "404", new string[] { "C#, UnityEngine" }, "4 Weeks", "404");
-        new ProjectDetails("Siepie and Takkie", " A project with Swedes and the Dutch", "SiepieAndTakkieOriginal", "404", new string[] { "C#, UnityEngine" }, "3 Weeks", "404");
-        new ProjectDetails("Siepie and Takkie Dialog system", "Finishing the dialog system", "Projects/SiepieAndTakkieDialogSystem", "images/projects/SiepieAndTakkieDialogDummy.png", new string[] { "C#", "Unity", "Jira" }, "6 Weeks", "https://github.com/Entropy-Entertainment/Siepie");
-        new ProjectDetails("Portfolio", "My personal portfolio website", "Projects/Portfolio", "images/projects/BlazorIcon.png", new string[] { "C#", "Blazor", "CSS" }, "2 Weeks", "404");
+        new ProjectDetails("The Journey", "My first console app in NodeJS", "Projects/TheJourney", "images/projects/Nathan.png", new string[] { "JS" }, "1 Week", "https://github.com/Nx27-ma/BO-textbased-application/");
+        new ProjectDetails("First basic website", "First web project I ever made", "https://37214.hosts2.ma-cloud.nl/Module2.1/Skill/LandingPage", "images/projects/dizzy.png", new string[]  { "HTML, CSS" }, "1 Week", "404");
+        new ProjectDetails("ColorTD", "Small tower defense game", "Projects/ColorTD", "images/projects/ColorTD.png", new string[] { "C#, UnityEngine, " }, "6 Weeks", "https://github.com/Nx27-ma/colorTD");
+        new ProjectDetails("Hollow Knight Slice", "A copy of a small part of the game", "Projects/HollowKnightSlice", "images/projects/HK.jpg", new string[] { "C#, UnityEngine" }, "4 Weeks", "https://github.com/Entropire/HungryNight");
+        new ProjectDetails("Siepie and Takkie", " A project with Swedes and the Dutch", "Projects/SiepieAndTakkieOriginal", "images/projects/SiepieTakkiePoster.png", new string[] { "C#, UnityEngine" }, "3 Weeks", "https://github.com/Nx27-ma/siepie");
+        new ProjectDetails("Entropy Jam", "A game jam I hosted", "Projects/EntropyJam", "images/projects/EntropyJam.jpg", new string[] { }, "3 day", "");
+        new ProjectDetails("Siepie and Takkie Dialog system", "Finishing the dialog system", "Projects/SiepieAndTakkieDialogSystem", "images/projects/SiepieTakkiePoster.png", new string[] { "C#", "Unity", "Jira" }, "6 Weeks", "https://github.com/Entropy-Entertainment/Siepie");
+        new ProjectDetails("Portfolio", "My personal portfolio website", "Projects/Portfolio", "images/projects/BlazorIcon.png", new string[] { "C#", "Blazor", "CSS" }, "Forever I suppose", "https://github.com/Nx27/PortfolioNXBI");
       }
       return AllProjects;
     }
