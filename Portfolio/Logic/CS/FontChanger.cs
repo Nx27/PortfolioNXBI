ccfdfec7d1df1f7ad7a31f77252d1690133c7af9
Nora de Greeuw <37214@ma-web.nl>
2026-04-21 14:50:56 +0200

    Updated ChunkSystemRefactor page + Added references to ProjectDetails

---
+++ Portfolio/Logic/CS/FontChanger.cs
@@ -0,0 +1,18 @@
+﻿namespace Portfolio.Logic.CS
+{
+  public class FontChanger
+  {
+    public event Action? OnChange;
+    private bool _altFont;
+    public bool AltFont
+    {
+      get => _altFont;
+      set
+      {
+        if (_altFont == value) return;
+        _altFont = value;
+        OnChange?.Invoke();
+      }
+    }
+  }
+}
--- Portfolio/Logic/CS/ProjectDetails.cs
+++ Portfolio/Logic/CS/ProjectDetails.cs
@@ -32,7 +32,7 @@
       AllProjects.Add(this);
     }
 
-    public static List<ProjectDetails> GetAllProjects()
+    public static Task<List<ProjectDetails>> GetAllProjects()
     {
       if (AllProjects.Count == 0)
       {
@@ -40,13 +40,14 @@
         new ProjectDetails("First basic website", "First web project I ever made", "Projects/FirstWebsite", "images/projects/dizzy.png", "1 Week", "404", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["HTML", "CSS"] } });
         new ProjectDetails("ColorTD", "Small tower defense game", "Projects/ColorTD", "images/projects/ColorTD.png", "6 Weeks", "https://github.com/Nx27-ma/colorTD", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#"] }, { FilterOptions.Frameworks, ["UnityEngine"] }, { FilterOptions.Software, ["Trello"] } });
         new ProjectDetails("Hollow Knight Slice", "A small copy of Hollow Knight", "Projects/HollowKnightSlice", "images/projects/HK.jpg", "4 Weeks", "https://github.com/Entropire/HungryNight", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#"] }, { FilterOptions.Frameworks, ["UnityEngine"] }, { FilterOptions.Software, ["Trello"] } });
-        new ProjectDetails("Siepie and Takkie", " A project with Swedes and the Dutch", "Projects/SiepieAndTakkieOriginal", "images/projects/siepietakkieoriginal/SiepieTakkiePoster.png", "3 Weeks", "https://github.com/Nx27-ma/siepie", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#"] }, { FilterOptions.Frameworks, ["UnityEngine"] } });
+        new ProjectDetails("Siepie and Takkie", "Swedish-Dutch joint project", "Projects/SiepieAndTakkieOriginal", "images/projects/siepietakkieoriginal/SiepieTakkiePoster.png", "3 Weeks", "https://github.com/Nx27-ma/siepie", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#"] }, { FilterOptions.Frameworks, ["UnityEngine"] } });
         new ProjectDetails("Entropy Jam", "A game jam I hosted", "Projects/EntropyJam", "images/projects/EntropyJam.jpg", "3 days", "", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, [] } });
         new ProjectDetails("Dialog system", "Finishing the dialog system", "Projects/SiepieAndTakkieDialogSystem", "images/projects/siepietakkieoriginal/SiepieTakkiePoster.png", "6 Weeks", "https://github.com/Entropy-Entertainment/Siepie", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#", "UXML"] }, { FilterOptions.Frameworks, ["UnityEngine", "NUnit"] }, { FilterOptions.Software, ["Jira"] } });
         new ProjectDetails("Portfolio", "My personal portfolio website", "Projects/Portfolio", "images/projects/BlazorIcon.png", "Forever I suppose", "https://github.com/Nx27/PortfolioNXBI", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#", "Razor", "HTML", "CSS"] }, { FilterOptions.Frameworks, ["Blazor WASM, ASP.NET"] }, { FilterOptions.Software, ["Adobe XD", "Photoshop"] } });
-        new ProjectDetails("Loading Screen", "A simple loading screen implementation", "Projects/LoadingScreen", "images/projects/loadingscreen/LoadingScreen.gif", "2 Days", "", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#"] }, { FilterOptions.Frameworks, ["UnityEngine"] }, { FilterOptions.Software, ["ClickUp"] } });
+        new ProjectDetails("Chunk System", "My biggest refactor ever", "Projects/ChunkSytemRefactor", "", "2 months", "", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#"] }, { FilterOptions.Frameworks, ["UnityEngine"] }, { FilterOptions.Software, ["ClickUp"] } });
+        new ProjectDetails("Loading Screen", "A loading screen for IBX", "Projects/LoadingScreen", "images/projects/loadingscreen/LoadingScreen.gif", "2 Days", "", new Dictionary<FilterOptions, string[]>() { { FilterOptions.Languages, ["C#"] }, { FilterOptions.Frameworks, ["UnityEngine"] }, { FilterOptions.Software, ["ClickUp"] } });
       }
-      return AllProjects;
+      return Task.FromResult(AllProjects); ;
     }
     public static List<ProjectDetails> GetByNames(params string[] names)
     {
